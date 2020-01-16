94ede02753fd00029404e55fe205c6cfb36bb98c
michael-weinstein <mweinste@ucla.edu>
2020-01-16 00:42:39 -0800

    Initial commit, ready for testing and debugging.  Fixed git ignoring the majority of files here.  Don't know why it did that in the first place.

---
+++ defaults/environment.py
@@ -0,0 +1,19 @@
+import os
+import datetime
+import __main__
+timestamp = str(datetime.datetime.now().timestamp()).replace(".", "")
+dataFolder = "/data"
+projectFolder = os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
+inputFolder = os.path.join(dataFolder, "input")
+workingFolder = os.path.join(dataFolder, "working")
+outputFolder = os.path.join(dataFolder, "output")
+sequenceFolder = os.path.join(inputFolder, "sequence")
+forwardReads = os.path.join(sequenceFolder, "standard_submitted_R1.fastq")
+reverseReads = os.path.join(sequenceFolder, "standard_submitted_R2.fastq")
+referenceFolder = os.path.join(os.path.split(__main__.__file__)[0], "reference")
+referenceGenome = os.path.join(referenceFolder, "zrGutStandard.fasta")
+#referenceGenome = "/opt/miqScoreShotgun/reference/zrGutStandard.fa" #for external debugging
+referenceDataFile = os.path.join(referenceFolder, "zrGutStandard.json")
+goodMiqExample = os.path.join(referenceFolder, "goodMiq.json")
+badMiqExample = os.path.join(referenceFolder, "badMiq.json")
+logFile = os.path.join(outputFolder, "dada2.%s.log" %timestamp)
